d9e42517845650520af04d0e1673e4795aa2e343
hajadelmas <52510008+hajadelmas@users.noreply.github.com>
2021-11-23 09:50:06 +0100

    test finish

---
+++ src/getDisplayName.test.ts
@@ -0,0 +1,20 @@
+import getDisplayName from './getDisplayName'
+
+describe('getDisplayName', ()=> {
+    describe('when no specified city', () => {
+        it('returns question mark and full name', ()=> {
+            expect(getDisplayName('Jane', 'Doe', '')).toBe('[?] Jane Doe')
+        })
+    })
+    describe('when specified city', () => {
+        it('returns city and full name', ()=> {
+            expect(getDisplayName('John', 'Doe', 'Paris')).toBe('[Paris] John Doe')
+        })
+    })
+    describe('when specified alter', () => {
+        it('returns city and full name', ()=> {
+            expect(getDisplayName('John', 'Doe', 'Paris', true)).toBe('[Paris] John Doe - Wns')
+        })
+    })
+    
+})
--- src/getDisplayName.ts
+++ src/getDisplayName.ts
@@ -1,5 +1,7 @@
-const getDisplayName = (firstName: string, lastName: string, city: string = '') => {
-    return ''
+const getDisplayName = (firstName: string, lastName: string, city: string = '', isWns: boolean = false) => {
+    const cityLabel = city || '?'
+    const wnsLabel = isWns ? ' - Wns' : ''
+    return `[${cityLabel}] ${firstName} ${lastName}${wnsLabel}`
 }
 
 export default getDisplayName
